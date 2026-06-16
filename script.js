d458da9db3251437d2d643659f49a48008d82396
AndreEnzo611 <andre.iriguchi@escola.pr.gov.br>
2026-06-16 12:17:13 -0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-// O java Script é uma linguagem de Web
+// Javacript é uma linguagem WEB
 
 // Variáveis - Valores armazenados na memória do computador
 
-//  Funções - Blocos de código
+// Funções - Blocos de códigos
 
-function carregar(){
-    //alert("Bem vindo ao JavaScript")
+function carregar (){
+    //alert("Bem-Vindo ao JavaScript")
+
     var msg = window.document.getElementById('msg')
-    var msg = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
 
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
+    var data = 17
 
-    msg.innerHTML = `Agora são ${hora}:${minutos}Am`
+    msg.innerHTML = `Agora são ${hora}:${minutos} AM`
+
+    if (hora >= 0 && hora < 12){
+        imag.src = 'manha.png'
+        document.body.style.background = 'Yellow'
+    }
+    else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.png'
+        document.body.style.background = 'Orange'
+    }
+    else{}
+
 }
